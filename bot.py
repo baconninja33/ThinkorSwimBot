6c5141208ddb40d8a9f7d43448673a983cbdf0a2
Hughes <guan.hughes@gmail.com>
2020-12-31 20:04:49 -0600

    Update to bot.py

--- bot.py
+++ bot.py
@@ -2,32 +2,31 @@
 #this is using paper wallet
 
 #import alpaca_trade_api as tradeapi
-import requests
+import requests, json
 from config import *
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
 ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID' API_KEY, 'APCA-API-SECRET-KEY': SECRET KEY}
 
-r = requests.get(account_url, headers={'APCA-API-KEY-ID':API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY})
+def get_account():
+    r = requests.get(ACCOUNT_URL, headers=HEADERS)
 
-print(r.content)
+    return json.loads(r.content)
 
-#class Martingale(object):
-   #def __init__(self):
-        #self.key = ''
-        #self.secret = ''
-       # self.alpaca_endpoint ='https://paper-api.alpaca.markets'
-       # self.symbol = 'IVV'
-        #self.current_order = None
-        #self.last_price = 1
+def create_order(symbol, qty, type, side, time_in_force):
+    data = {
+        "symbol": symbol,
+        "qty": qty,
+        "side": side,
+        "type": type,
+        "time_in_force": time_in_force
+    }
 
-    #try:
-       # self.position = int(self.api.get_position(self.symbol).qty)
-   # except:
-      #  self.position = 0
+    r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
 
-#def submit_order(self, target):
-    #if self.current_order is not None:
-        #self.api.cancel_order(self.current_order.id)
+    return json.loads(r.content)
 
-    
+response = create_order("AAPL", 100,"buy", "market", "gtc")
+
+print(response)
